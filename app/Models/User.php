68523d2bd8e12145796190680db391532f82877c
gysmoy <gamboapalominocarlosmanuel@gmail.com>
2025-01-24 11:19:04 -0500

    NTS

--- app/Models/User.php
+++ app/Models/User.php
@@ -134,6 +134,7 @@ class User extends Authenticatable
                 'products.creditos',
                 'products.duracion',
                 'products.fecha_inicio',
+                'products.fecha_fin',
                 'products.beneficios',
                 'products.curso_dirigido',
                 'products.description2',
